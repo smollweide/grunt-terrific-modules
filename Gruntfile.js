9c1dfc017ee5e5756cf90631ddc3df8a46eb26f3
Simon Mollweide <simon.mollweide@namics.com>
2014-12-11 11:00:25 +0100

    add options in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,22 +30,79 @@ module.exports = function (grunt) {
 
 		// Configuration to be run (and then tested).
 		terrific_modules: {
-			default_options: {
-				options: {
-				},
-				files: {
-					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+			options: {
+				placeholder: {
+					module: {
+						underscore: 'tcdefault',
+						camelCase: 'Tcdefault'
+					},
+					skin: {
+						underscore: 'skindefault',
+						camelCase: 'Skindefault'
+					},
+					template: {
+						underscore: 'templatedefault',
+						camelCase: 'Templatedefault'
+					},
+					author: '{author}'
 				}
 			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!'
+			filesModule: [
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault',
+					template: 'tcdefault.jsp'
 				},
-				files: {
-					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault',
+					template: 'tcdefault.readme.md'
+				},
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault/js',
+					template: 'tcdefault.js'
+				},
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault/css',
+					template: 'tcdefault.less'
+				},
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault/i18n',
+					template: 'tcdefault.properties'
+				},
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out_tag%>',
+					template: 'tcdefault.tag'
 				}
-			}
+			],
+			filesSkin: [
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault/js',
+					template: 'tcdefault.skin.skindefault.js'
+				},
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault/css',
+					template: 'tcdefault.skin.skindefault.less'
+				}
+			],
+			filesTemplate: [
+				{
+					src: '<%=app.paths.generator_resource%>',
+					dest: '<%=app.paths.generator_out%>/tcdefault',
+					template: 'tcdefault-templatedefault.jsp',
+					belongsTo: {
+						src: '<%=app.paths.generator_out_tag%>/tcdefault.tag',
+						placeholder: '<%-- outlet.template --%>',
+						template: '<%=app.paths.generator_resource%>/tcdefault.template.tag'
+					}
+				}
+			]
 		},
 
 		// Unit tests.
